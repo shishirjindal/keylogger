710865251ef6f7f76b10f9e4ac9b29f289427fbc
Shishir Jindal <honey.jindal5@gmail.com>
2017-05-14 20:55:06 +0530

    Added feature of special chars.

--- main.c
+++ main.c
@@ -10,9 +10,10 @@
 #define CAPSLOCK 58
 #define ROOTID 0
 
-// arrays for keys(for both small and capital chars)
-static char *keys_upper[] = {"RESERVED", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "BACKSPACE", "	", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "ENTER", "LEFTCTRL", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", "LEFTSHIFT", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "RIGHTSHIFT", "*", "LEFTALT", " ", "CAPSLOCK", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "NUMLOCK", "SCROLLLOCK", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "0", ".", "ZENKAKUHANKAKU", "102ND", "F11", "F12", "RO", "KATAKANA", "HIRAGANA", "HENKAN", "KATAKANAHIRAGANA", "MUHENKAN", "JPCOMMA", "ENTER", "RIGHTCTRL", "/", "SYSRQ", "RIGHTALT", "LINEFEED", "HOME", "UP", "PAGEUP", "LEFT", "RIGHT", "END", "DOWN", "PAGEDOWN", "INSERT", "DELETE", "MACRO", "MUTE", "VOLUMEDOWN", "VOLUMEUP", "POWER"};
-static char *keys_lower[] = {"reserved", "esc", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace", "	", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "{", "}", "enter", "leftctrl", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "`", "leftshift", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "rightshift", "*", "leftalt", " ", "capslock", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "numlock", "scrolllock", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "0", ".", "zenkakuhankaku", "102nd", "f11", "f12", "ro", "katakana", "hiragana", "henkan", "katakanahiragana", "muhenkan", "jpcomma", "enter", "rightctrl", "/", "sysrq", "rightalt", "linefeed", "home", "up", "pageup", "left", "right", "end", "down", "pagedown", "insert", "delete", "macro", "mute", "volumedown", "volumeup", "power"};
+// arrays for keys(for both small and capital chars including chars like !@#$%^&*())
+static char *keys_upper[] = {"<RESERVED>", "<ESC>", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "<BACKSPACE>", "	", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\n", "<LEFTCTRL>", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", "<LEFTSHIFT>", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "<RIGHTSHIFT>", "*", "<LEFTALT>", " ", "<CAPSLOCK>", "<F1>", "<F2>", "<F3>", "<F4>", "<F5>", "<F6>", "<F7>", "<F8>", "<F9>", "<F10>", "<NUMLOCK>", "<SCROLLLOCK>", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "0", ".", "" ,"<ZENKAKUHANKAKU>", "102ND", "<F11>", "<F12>", "<RO>", "<KATAKANA>", "<HIRAGANA>", "<HENKAN>", "<KATAKANAHIRAGANA>", "<MUHENKAN>", "<JPCOMMA>", "\n", "<RIGHTCTRL>", "/", "<SYSRQ>", "<RIGHTALT>", "<LINEFEED>", "<HOME>", "<UP>", "<PAGEUP>", "<LEFT>", "<RIGHT>", "<END>", "<DOWN>", "<PAGEDOWN>", "<INSERT>", "<DELETE>", "<MACRO>", "<MUTE>", "<VOLUMEDOWN>", "<VOLUMEUP>", "<POWER>"};
+static char *keys_lower[] = {"<reserved>", "<esc>", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "<backspace>", "	", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\n", "<leftctrl>", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "`", "<leftshift>", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "<rightshift>", "*", "<leftalt>", " ", "<capslock>", "<f1>", "<f2>", "<f3>", "<f4>", "<f5>", "<f6>", "<f7>", "<f8>", "<f9>", "<f10>", "<numlock>", "<scrolllock>", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "0", ".", "" ,"<zenkakuhankaku>", "102nd", "<f11>", "<f12>", "<ro>", "<katakana>", "<hiragana>", "<henkan>", "<katakanahiragana>", "<muhenkan>", "<jpcomma>", "\n", "<rightctrl>", "/", "<sysrq>", "<rightalt>", "<linefeed>", "<home>", "<up>", "<pageup>", "<left>", "<right>", "<end>", "<down>", "<pagedown>", "<insert>", "<delete>", "<macro>", "<mute>", "<volumedown>", "<volumeup>", "<power>"};
+static char *special_chars[2][20] = {{"`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "[", "]", ";", "'", ",", ".", "/"},{"~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", ":", "\"", "<", ">", "?"}};
 
 // checking for shift key
 static int isShiftPressed(int eventcode){
@@ -30,10 +31,24 @@ static int isCapsOn(int eventcode){
 	return 0;
 }
 
+// check for chars like !@#$%^&*()_
+static char *checkKey(char *key){
+	for(int i=0; i<20; i++){
+		if(special_chars[0][i] == key){
+			return special_chars[1][i];
+		}
+	}
+	return key;
+}
+
 // convert event code to corresponding char
 static char *pressedKey(int eventcode, int shift, int caps){
 	if(shift^caps){
-		return keys_upper[eventcode];
+		char *key = keys_upper[eventcode];
+		if(shift == 1){
+			key = checkKey(key);
+		}
+		return key;
 	}
 	return keys_lower[eventcode];
 }
